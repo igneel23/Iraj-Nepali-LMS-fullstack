39a0f0870d598e3f889d3f2fbc0c4d6d9d1e6332
igneel23 <irajnepali8@gmail.com>
2026-04-03 08:30:40 +0545

    Commit made

--- build_files.sh
+++ build_files.sh
@@ -5,11 +5,18 @@ set -e
 
 echo "--- BUILDING PROJECT ---"
 
-# Install dependencies
-echo "Installing pip dependencies..."
-python3 -m pip install -r requirements.txt
+# 1. Create a virtual environment for the build process
+# This avoids the "externally-managed-environment" error on Vercel
+echo "Setting up virtual environment..."
+python3 -m venv venv_build
+source venv_build/bin/activate
 
-# Run Management Commands
+# 2. Upgrade pip and install dependencies 
+echo "Installing pip dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# 3. Run Django Management Commands
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
 
@@ -17,4 +24,3 @@ echo "Running migrations..."
 python3 manage.py migrate --noinput
 
 echo "--- BUILD COMPLETE ---"
-
